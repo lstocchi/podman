70950962a0f8e8dcf909abf98eb5b9f8087f94a7
Christophe Fergeau <cfergeau@redhat.com>
2025-01-31 11:53:19 +0100

    machine: Make podman socket forwarding optional
    podman-machine is forwarding some podman-specific sockets from the VM to
    the host, which requires ssh access to the VM and the use of gvproxy.
    This is not needed by macadam, and is currently detrimental as we want
    to allow to run unmodified VM images.
    This commit adds a new `Capabilities` struct to the vmconfigs, which
    will be used for such optional features. podman-machine will enable
    socket forwarding, but macadam will be able to opt out of it, and the
    podman-machine code is modified to only try to forward sockets when the
    capability is enabled.

--- pkg/machine/shim/host.go
+++ pkg/machine/shim/host.go
@@ -83,6 +83,12 @@ func Init(opts machineDefine.InitOptions, mp vmconfigs.VMProvider) error {
 		return err
 	}
 
+	if opts.Capabilities == nil {
+		var defaultCapabilities *machineDefine.MachineCapabilities = nil
+		opts.Capabilities = &machineDefine.MachineCapabilities{
+			ForwardSockets: defaultCapabilities.GetForwardSockets(),
+		}
+	}
 	/* check the path to env.GetSSHIdentityPath */
 	/* ----> Can we make it configurable in initopts? */
 	/* Can we dynamically update vmconfig.SSH ? */
@@ -112,6 +118,7 @@ func Init(opts machineDefine.InitOptions, mp vmconfigs.VMProvider) error {
 	}
 
 	mc.Version = vmconfigs.MachineConfigVersion
+	mc.Capabilities = opts.Capabilities
 
 	createOpts := machineDefine.CreateVMOpts{
 		Name: opts.Name,
@@ -250,18 +257,20 @@ func Init(opts machineDefine.InitOptions, mp vmconfigs.VMProvider) error {
 	}
 
 	// TODO AddSSHConnectionToPodmanSocket could take an machineconfig instead
-	if err := connection.AddSSHConnectionsToPodmanSocket(mc.HostUser.UID, mc.SSH.Port, mc.SSH.IdentityPath, mc.Name, mc.SSH.RemoteUsername, opts); err != nil {
-		return err
-	}
-
-	cleanup := func() error {
-		machines, err := provider.GetAllMachinesAndRootfulness()
-		if err != nil {
+	if mc.Capabilities.GetForwardSockets() {
+		if err := connection.AddSSHConnectionsToPodmanSocket(mc.HostUser.UID, mc.SSH.Port, mc.SSH.IdentityPath, mc.Name, mc.SSH.RemoteUsername, opts); err != nil {
 			return err
 		}
-		return connection.RemoveConnections(machines, mc.Name, mc.Name+"-root")
+
+		cleanup := func() error {
+			machines, err := provider.GetAllMachinesAndRootfulness()
+			if err != nil {
+				return err
+			}
+			return connection.RemoveConnections(machines, mc.Name, mc.Name+"-root")
+		}
+		callbackFuncs.Add(cleanup)
 	}
-	callbackFuncs.Add(cleanup)
 
 	logrus.Warn("CreateVM")
 	err = mp.CreateVM(createOpts, mc, &ignBuilder)
