8bac139ef17334a0814ca53bd81f1df7d7590982
Christophe Fergeau <cfergeau@redhat.com>
2026-01-23 10:31:28 +0100

    move GenerateUserData to os specific impl
    to support user-mode networking in hyperv we need to customize the
    cloud-init config. This patch just move GenerateUserData outside of the
    default cloudinit impl so it can be customized for hyperv in following
    commit.
    
    Signed-off-by: lstocchi <lstocchi@redhat.com>

---
+++ pkg/machine/cloudinit/cloudinit_unix.go
@@ -0,0 +1,40 @@
+//go:build !windows
+
+package cloudinit
+
+import (
+	"github.com/containers/podman/v6/pkg/machine"
+	"github.com/containers/podman/v6/pkg/machine/vmconfigs"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+func GenerateUserData(mc *vmconfigs.MachineConfig) ([]byte, error) {
+	sshKey, err := machine.GetSSHKeys(mc.SSH.IdentityPath)
+	if err != nil {
+		return nil, err
+	}
+
+	userData := UserData{
+		Users: []User{
+			User{
+				Name:    mc.SSH.RemoteUsername,
+				Sudo:    "ALL=(ALL) NOPASSWD:ALL",
+				Shell:   "/bin/bash",
+				Groups:  []string{"users"},
+				SSHKeys: []string{sshKey},
+			},
+		},
+	}
+
+	yamlBytes, err := yaml.Marshal(&userData)
+	if err != nil {
+		logrus.Errorf("Error marshaling to YAML: %v", err)
+		return nil, err
+	}
+
+	headerLine := "#cloud-config\n"
+	yamlBytes = append([]byte(headerLine), yamlBytes...)
+
+	return yamlBytes, nil
+}
