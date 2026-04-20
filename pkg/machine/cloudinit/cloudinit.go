crc-org/podman-macadam

machine: Return VMFile from cloud-init.GenerateISO()

pkg/machine/cloudinit/cloudinit.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/containers/podman/v5/pkg/machine"
+	"github.com/containers/podman/v5/pkg/machine/define"
 	"github.com/containers/podman/v5/pkg/machine/vmconfigs"
 	"github.com/kdomanski/iso9660"
 	"github.com/sirupsen/logrus"
@@ -78,10 +79,10 @@ func GenerateUserDataFile(mc *vmconfigs.MachineConfig) (string, error) {
 	return path, nil
 }
 
-func GenerateISO(mc *vmconfigs.MachineConfig) (string, error) {
+func GenerateISO(mc *vmconfigs.MachineConfig) (*define.VMFile, error) {
 	writer, err := iso9660.NewWriter()
 	if err != nil {
-		return "", fmt.Errorf("failed to create writer: %w", err)
+		return nil, fmt.Errorf("failed to create writer: %w", err)
 	}
 
 	defer func() {
@@ -92,25 +93,28 @@ func GenerateISO(mc *vmconfigs.MachineConfig) (string, error) {
 
 	userdata, err := GenerateUserData(mc)
 	if err != nil {
-		return "", nil
+		return nil, nil
 	}
 	if err := writer.AddFile(bytes.NewReader(userdata), "user-data"); err != nil {
-		return "", err
+		return nil, err
 	}
 	if err := writer.AddFile(bytes.NewReader([]byte{}), "meta-data"); err != nil {
-		return "", err
+		return nil, err
 	}
 
 	machineDataDir, err := mc.DataDir()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	path := filepath.Join(machineDataDir.GetPath(), "cloudinit.iso")
-
-	isoFile, err := os.Create(path)
+	vmFile, err := machineDataDir.AppendToNewVMFile(("cloudinit.iso"), nil)
 	if err != nil {
-		return "", fmt.Errorf("unable to create cloud-init ISO file: %w", err)
+		return nil, err
+	}
+
+	isoFile, err := os.Create(vmFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("unable to create cloud-init ISO file: %w", err)
 	}
 
 	defer func() {
@@ -122,8 +126,8 @@ func GenerateISO(mc *vmconfigs.MachineConfig) (string, error) {
 	err = writer.WriteTo(isoFile, "cidata")
 	if err != nil {
 		os.Remove(isoFile.Name())
-		return "", fmt.Errorf("failed to write cloud-init ISO image: %w", err)
+		return nil, fmt.Errorf("failed to write cloud-init ISO image: %w", err)
 	}
 
-	return isoFile.Name(), nil
+	return vmFile, nil
 }

lstocchi/podman

pkg/machine/cloudinit/cloudinit.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 
 	"github.com/containers/podman/v6/pkg/machine"
+	"github.com/containers/podman/v6/pkg/machine/define"
 	"github.com/containers/podman/v6/pkg/machine/vmconfigs"
 	"github.com/kdomanski/iso9660"
 	"github.com/sirupsen/logrus"
@@ -78,10 +79,10 @@ func GenerateUserDataFile(mc *vmconfigs.MachineConfig) (string, error) {
 	return path, nil
 }
 
-func GenerateISO(mc *vmconfigs.MachineConfig) (string, error) {
+func GenerateISO(mc *vmconfigs.MachineConfig) (*define.VMFile, error) {
 	writer, err := iso9660.NewWriter()
 	if err != nil {
-		return "", fmt.Errorf("failed to create writer: %w", err)
+		return nil, fmt.Errorf("failed to create writer: %w", err)
 	}
 
 	defer func() {
@@ -92,25 +93,28 @@ func GenerateISO(mc *vmconfigs.MachineConfig) (string, error) {
 
 	userdata, err := GenerateUserData(mc)
 	if err != nil {
-		return "", nil
+		return nil, nil
 	}
 	if err := writer.AddFile(bytes.NewReader(userdata), "user-data"); err != nil {
-		return "", err
+		return nil, err
 	}
 	if err := writer.AddFile(bytes.NewReader([]byte{}), "meta-data"); err != nil {
-		return "", err
+		return nil, err
 	}
 
 	machineDataDir, err := mc.DataDir()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	path := filepath.Join(machineDataDir.GetPath(), "cloudinit.iso")
-
-	isoFile, err := os.Create(path)
+	vmFile, err := machineDataDir.AppendToNewVMFile(("cloudinit.iso"), nil)
 	if err != nil {
-		return "", fmt.Errorf("unable to create cloud-init ISO file: %w", err)
+		return nil, err
+	}
+
+	isoFile, err := os.Create(vmFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("unable to create cloud-init ISO file: %w", err)
 	}
 
 	defer func() {
@@ -122,8 +126,8 @@ func GenerateISO(mc *vmconfigs.MachineConfig) (string, error) {
 	err = writer.WriteTo(isoFile, "cidata")
 	if err != nil {
 		os.Remove(isoFile.Name())
-		return "", fmt.Errorf("failed to write cloud-init ISO image: %w", err)
+		return nil, fmt.Errorf("failed to write cloud-init ISO image: %w", err)
 	}
 
-	return isoFile.Name(), nil
+	return vmFile, nil
 }