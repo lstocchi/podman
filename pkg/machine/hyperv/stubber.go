crc-org/podman-macadam

delete cloudInit ISO file when VM is removed
when using cloud-init an ISO with all config files is generated to be used when the VM gets started. However this file is never deleted even when the VM is removed.
This patch fixes it and handle the deletion of the cloud ISO file.

Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

pkg/machine/hyperv/stubber.go
@@ -19,6 +19,7 @@ import (
 	"github.com/containers/common/pkg/strongunits"
 	gvproxy "github.com/containers/gvisor-tap-vsock/pkg/types"
 	"github.com/containers/libhvee/pkg/hypervctl"
+	"github.com/containers/podman/v5/pkg/errorhandling"
 	"github.com/containers/podman/v5/pkg/machine"
 	"github.com/containers/podman/v5/pkg/machine/cloudinit"
 	"github.com/containers/podman/v5/pkg/machine/define"
@@ -182,7 +183,16 @@ func (h HyperVStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() err
 		return nil, nil, err
 	}
 
+	rmFiles := []string{}
+
+	cloudinitISO, err := cloudinit.GetCloudInitISOVMFile(mc)
+	if err == nil {
+		rmFiles = append(rmFiles, cloudinitISO.GetPath())
+	}
+
 	rmFunc := func() error {
+		var errs []error
+
 		// Tear down vsocks
 		removeNetworkAndReadySocketsFromRegistry(mc)
 
@@ -191,12 +201,23 @@ func (h HyperVStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() err
 		// TODO we could improve this by recommending an action be done
 		if !mc.CloudInit {
 			if err := removeIgnitionFromRegistry(vm); err != nil {
-				logrus.Errorf("unable to remove ignition registry entries: %q", err)
+				errs = append(errs, fmt.Errorf("unable to remove ignition registry entries: %q", err))
+			}
+		}
+
+		if cloudinitISO != nil {
+			if err := cloudinitISO.Delete(); err != nil {
+				errs = append(errs, err)
 			}
 		}
 
 		// disk path removal is done by generic remove
-		return vm.Remove("")
+		err := vm.Remove("")
+		if err != nil {
+			errs = append(errs, err)
+		}
+
+		return errorhandling.JoinErrors(errs)
 	}
 	return []string{}, rmFunc, nil
 }

pkg/machine/qemu/machine.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/containers/podman/v5/pkg/errorhandling"
+	"github.com/containers/podman/v5/pkg/machine/cloudinit"
 	"github.com/containers/podman/v5/pkg/machine/define"
 	"github.com/containers/podman/v5/pkg/machine/vmconfigs"
 	"github.com/containers/storage/pkg/fileutils"
@@ -230,6 +231,11 @@ func (q *QEMUStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() erro
 		mc.QEMUHypervisor.QMPMonitor.Address.GetPath(),
 	}
 
+	cloudinitISO, err := cloudinit.GetCloudInitISOVMFile(mc)
+	if err == nil {
+		qemuRmFiles = append(qemuRmFiles, cloudinitISO.GetPath())
+	}
+
 	return qemuRmFiles, func() error {
 		var errs []error
 		if err := mc.QEMUHypervisor.QEMUPidPath.Delete(); err != nil {
@@ -239,6 +245,13 @@ func (q *QEMUStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() erro
 		if err := mc.QEMUHypervisor.QMPMonitor.Address.Delete(); err != nil {
 			errs = append(errs, err)
 		}
+
+		if cloudinitISO != nil {
+			if err := cloudinitISO.Delete(); err != nil {
+				errs = append(errs, err)
+			}
+		}
+
 		return errorhandling.JoinErrors(errs)
 	}, nil
 }

lstocchi/podman

pkg/machine/hyperv/stubber.go
@@ -19,6 +19,7 @@ import (
 	gvproxy "github.com/containers/gvisor-tap-vsock/pkg/types"
 	"github.com/containers/libhvee/pkg/hypervctl"
 	"github.com/containers/libhvee/pkg/kvp/ginsu"
+	"github.com/containers/podman/v6/pkg/errorhandling"
 	"github.com/containers/podman/v6/pkg/machine"
 	"github.com/containers/podman/v6/pkg/machine/cloudinit"
 	"github.com/containers/podman/v6/pkg/machine/define"
@@ -244,19 +245,35 @@ func (h HyperVStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() err
 		return nil, nil, err
 	}
 
+	rmFiles := []string{}
+
+	cloudinitISO, err := cloudinit.GetCloudInitISOVMFile(mc)
+	if err == nil {
+		rmFiles = append(rmFiles, cloudinitISO.GetPath())
+	}
+
 	rmFunc := func() error {
+		var errs []error
+
 		// Remove ignition registry entries - not a fatal error
 		// for vm removal
 		// TODO we could improve this by recommending an action be done
 		if !mc.CloudInit {
 			if err := removeIgnitionFromRegistry(mc, vm); err != nil {
-				logrus.Errorf("unable to remove ignition registry entries: %q", err)
+				errs = append(errs, fmt.Errorf("unable to remove ignition registry entries: %q", err))
+			}
+		}
+
+		if cloudinitISO != nil {
+			if err := cloudinitISO.Delete(); err != nil {
+				errs = append(errs, err)
 			}
 		}
 
 		// disk path removal is done by generic remove
 		if err = vm.Remove(""); err != nil {
-			return err
+			errs = append(errs, err)
+			return errorhandling.JoinErrors(errs)
 		}
 
 		// remove vsock registry entries
@@ -268,7 +285,7 @@ func (h HyperVStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() err
 			}
 		}
 
-		return nil
+		return errorhandling.JoinErrors(errs)
 	}
 	return []string{}, rmFunc, nil
 }

pkg/machine/qemu/machine.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/containers/podman/v6/pkg/errorhandling"
+	"github.com/containers/podman/v6/pkg/machine/cloudinit"
 	"github.com/containers/podman/v6/pkg/machine/define"
 	"github.com/containers/podman/v6/pkg/machine/vmconfigs"
 	"github.com/digitalocean/go-qemu/qmp"
@@ -230,6 +231,11 @@ func (q *QEMUStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() erro
 		mc.QEMUHypervisor.QMPMonitor.Address.GetPath(),
 	}
 
+	cloudinitISO, err := cloudinit.GetCloudInitISOVMFile(mc)
+	if err == nil {
+		qemuRmFiles = append(qemuRmFiles, cloudinitISO.GetPath())
+	}
+
 	return qemuRmFiles, func() error {
 		var errs []error
 		if err := mc.QEMUHypervisor.QEMUPidPath.Delete(); err != nil {
@@ -239,6 +245,13 @@ func (q *QEMUStubber) Remove(mc *vmconfigs.MachineConfig) ([]string, func() erro
 		if err := mc.QEMUHypervisor.QMPMonitor.Address.Delete(); err != nil {
 			errs = append(errs, err)
 		}
+
+		if cloudinitISO != nil {
+			if err := cloudinitISO.Delete(); err != nil {
+				errs = append(errs, err)
+			}
+		}
+
 		return errorhandling.JoinErrors(errs)
 	}, nil
 }