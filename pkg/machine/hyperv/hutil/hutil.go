crc-org/podman-macadam

update visibility createNetworkUnit and hyperVVsockNMConnection in hyperv stubber
this commit updates the visibility of createNetworkUnit and
hyperVVsockNMConnection and move it to another package so they can be reused
by the cloudinit package in further commit.

Signed-off-by: lstocchi <lstocchi@redhat.com>

pkg/machine/hyperv/hutil/hutil.go
@@ -1,0 +1,35 @@
+package hutil
+
+import (
+	"fmt"
+
+	"github.com/containers/podman/v5/pkg/systemd/parser"
+)
+
+const HyperVVsockNMConnection = `
+[connection]
+id=vsock0
+type=tun
+interface-name=vsock0
+
+[tun]
+mode=2
+
+[802-3-ethernet]
+cloned-mac-address=5A:94:EF:E4:0C:EE
+
+[ipv4]
+method=auto
+
+[proxy]
+`
+
+func CreateNetworkUnit(netPort uint64) (string, error) {
+	netUnit := parser.NewUnitFile()
+	netUnit.Add("Unit", "Description", "vsock_network")
+	netUnit.Add("Unit", "After", "NetworkManager.service")
+	netUnit.Add("Service", "ExecStart", fmt.Sprintf("/usr/libexec/podman/gvforwarder -preexisting -iface vsock0 -url vsock://2:%d/connect", netPort))
+	netUnit.Add("Service", "ExecStartPost", "/usr/bin/nmcli c up vsock0")
+	netUnit.Add("Install", "WantedBy", "multi-user.target")
+	return netUnit.ToString()
+}

pkg/machine/hyperv/stubber.go
@@ -24,10 +24,10 @@ import (
 	"github.com/containers/podman/v5/pkg/machine/cloudinit"
 	"github.com/containers/podman/v5/pkg/machine/define"
 	"github.com/containers/podman/v5/pkg/machine/env"
+	"github.com/containers/podman/v5/pkg/machine/hyperv/hutil"
 	"github.com/containers/podman/v5/pkg/machine/hyperv/vsock"
 	"github.com/containers/podman/v5/pkg/machine/ignition"
 	"github.com/containers/podman/v5/pkg/machine/vmconfigs"
-	"github.com/containers/podman/v5/pkg/systemd/parser"
 	"github.com/sirupsen/logrus"
 )
 
@@ -119,7 +119,7 @@ func (h HyperVStubber) CreateVM(opts define.CreateVMOpts, mc *vmconfigs.MachineC
 	callbackFuncs.Add(removeRegistrySockets)
 
 	if builder != nil {
-		netUnitFile, err := createNetworkUnit(mc.HyperVHypervisor.NetworkVSock.Port)
+		netUnitFile, err := hutil.CreateNetworkUnit(mc.HyperVHypervisor.NetworkVSock.Port)
 		if err != nil {
 			return err
 		}
@@ -137,7 +137,7 @@ func (h HyperVStubber) CreateVM(opts define.CreateVMOpts, mc *vmconfigs.MachineC
 			FileEmbedded1: ignition.FileEmbedded1{
 				Append: nil,
 				Contents: ignition.Resource{
-					Source: ignition.EncodeDataURLPtr(hyperVVsockNMConnection),
+					Source: ignition.EncodeDataURLPtr(hutil.HyperVVsockNMConnection),
 				},
 				Mode: ignition.IntToPtr(0600),
 			},
@@ -646,34 +646,6 @@ func logCommandToFile(c *exec.Cmd, filename string) (*os.File, error) {
 	c.Stderr = log
 
 	return log, nil
-}
-
-const hyperVVsockNMConnection = `
-[connection]
-id=vsock0
-type=tun
-interface-name=vsock0
-
-[tun]
-mode=2
-
-[802-3-ethernet]
-cloned-mac-address=5A:94:EF:E4:0C:EE
-
-[ipv4]
-method=auto
-
-[proxy]
-`
-
-func createNetworkUnit(netPort uint64) (string, error) {
-	netUnit := parser.NewUnitFile()
-	netUnit.Add("Unit", "Description", "vsock_network")
-	netUnit.Add("Unit", "After", "NetworkManager.service")
-	netUnit.Add("Service", "ExecStart", fmt.Sprintf("/usr/libexec/podman/gvforwarder -preexisting -iface vsock0 -url vsock://2:%d/connect", netPort))
-	netUnit.Add("Service", "ExecStartPost", "/usr/bin/nmcli c up vsock0")
-	netUnit.Add("Install", "WantedBy", "multi-user.target")
-	return netUnit.ToString()
 }
 
 func (h HyperVStubber) GetRosetta(mc *vmconfigs.MachineConfig) (bool, error) {

lstocchi/podman

pkg/machine/hyperv/hutil/hutil.go
@@ -1,0 +1,35 @@
+package hutil
+
+import (
+	"fmt"
+
+	"github.com/containers/podman/v6/pkg/systemd/parser"
+)
+
+const HyperVVsockNMConnection = `
+[connection]
+id=vsock0
+type=tun
+interface-name=vsock0
+
+[tun]
+mode=2
+
+[802-3-ethernet]
+cloned-mac-address=5A:94:EF:E4:0C:EE
+
+[ipv4]
+method=auto
+
+[proxy]
+`
+
+func CreateNetworkUnit(netPort uint64) (string, error) {
+	netUnit := parser.NewUnitFile()
+	netUnit.Add("Unit", "Description", "vsock_network")
+	netUnit.Add("Unit", "After", "NetworkManager.service")
+	netUnit.Add("Service", "ExecStart", fmt.Sprintf("/usr/libexec/podman/gvforwarder -preexisting -iface vsock0 -url vsock://2:%d/connect", netPort))
+	netUnit.Add("Service", "ExecStartPost", "/usr/bin/nmcli c up vsock0")
+	netUnit.Add("Install", "WantedBy", "multi-user.target")
+	return netUnit.ToString()
+}

pkg/machine/hyperv/stubber.go
@@ -24,11 +24,11 @@ import (
 	"github.com/containers/podman/v6/pkg/machine/cloudinit"
 	"github.com/containers/podman/v6/pkg/machine/define"
 	"github.com/containers/podman/v6/pkg/machine/env"
+	"github.com/containers/podman/v6/pkg/machine/hyperv/hutil"
 	"github.com/containers/podman/v6/pkg/machine/hyperv/vsock"
 	"github.com/containers/podman/v6/pkg/machine/ignition"
 	"github.com/containers/podman/v6/pkg/machine/vmconfigs"
 	"github.com/containers/podman/v6/pkg/machine/windows"
-	"github.com/containers/podman/v6/pkg/systemd/parser"
 	"github.com/sirupsen/logrus"
 	"go.podman.io/common/pkg/strongunits"
 )
@@ -163,7 +163,7 @@ func (h HyperVStubber) CreateVM(_ define.CreateVMOpts, mc *vmconfigs.MachineConf
 	}
 
 	if builder != nil {
-		netUnitFile, err := createNetworkUnit(mc.HyperVHypervisor.NetworkVSock.Port)
+		netUnitFile, err := hutil.CreateNetworkUnit(mc.HyperVHypervisor.NetworkVSock.Port)
 		if err != nil {
 			return err
 		}
@@ -181,7 +181,7 @@ func (h HyperVStubber) CreateVM(_ define.CreateVMOpts, mc *vmconfigs.MachineConf
 			FileEmbedded1: ignition.FileEmbedded1{
 				Append: nil,
 				Contents: ignition.Resource{
-					Source: ignition.EncodeDataURLPtr(hyperVVsockNMConnection),
+					Source: ignition.EncodeDataURLPtr(hutil.HyperVVsockNMConnection),
 				},
 				Mode: ignition.IntToPtr(0o600),
 			},
@@ -887,34 +887,6 @@ func logCommandToFile(c *exec.Cmd, filename string) (*os.File, error) {
 	c.Stderr = log
 
 	return log, nil
-}
-
-const hyperVVsockNMConnection = `
-[connection]
-id=vsock0
-type=tun
-interface-name=vsock0
-
-[tun]
-mode=2
-
-[802-3-ethernet]
-cloned-mac-address=5A:94:EF:E4:0C:EE
-
-[ipv4]
-method=auto
-
-[proxy]
-`
-
-func createNetworkUnit(netPort uint64) (string, error) {
-	netUnit := parser.NewUnitFile()
-	netUnit.Add("Unit", "Description", "vsock_network")
-	netUnit.Add("Unit", "After", "NetworkManager.service")
-	netUnit.Add("Service", "ExecStart", fmt.Sprintf("/usr/libexec/podman/gvforwarder -preexisting -iface vsock0 -url vsock://2:%d/connect", netPort))
-	netUnit.Add("Service", "ExecStartPost", "/usr/bin/nmcli c up vsock0")
-	netUnit.Add("Install", "WantedBy", "multi-user.target")
-	return netUnit.ToString()
 }
 
 func (h HyperVStubber) GetRosetta(_ *vmconfigs.MachineConfig) (bool, error) {